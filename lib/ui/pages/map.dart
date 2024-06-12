53585ea1fb33cd86cb3cf334d8ef2ae57dea9e78
Faith Omotoso <faithomotoso@gmail.com>
2024-06-12 09:42:15 +0100

    Animations for search, filter & button widgets in map view

--- lib/ui/pages/map.dart
+++ lib/ui/pages/map.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 import 'dart:developer';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:m_point_assessment/ui/utils/asset_paths.dart';
@@ -16,18 +18,30 @@ class MapView extends StatefulWidget {
   State<MapView> createState() => _MapViewState();
 }
 
-class _MapViewState extends State<MapView> {
-  final ValueNotifier<GoogleMapController?> _mapController =
+class _MapViewState extends State<MapView> with TickerProviderStateMixin {
+  final ValueNotifier<GoogleMapController?> mapController =
       ValueNotifier<GoogleMapController?>(null);
   bool isStyleSet = false;
+
+  late final AnimationController sizeAnimationController;
 
   @override
   void initState() {
     super.initState();
 
-    _mapController.addListener(() {
+    sizeAnimationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 600));
+
+    mapController.addListener(() {
       _loadMapTheme();
     });
+  }
+
+  @override
+  void dispose() {
+    mapController.value?.dispose();
+    sizeAnimationController.dispose();
+    super.dispose();
   }
 
   void _loadMapTheme() async {
@@ -36,8 +50,10 @@ class _MapViewState extends State<MapView> {
         String mapStyle =
             await rootBundle.loadString("assets/map_theme/map_theme.json");
 
-        _mapController.value?.setMapStyle(mapStyle);
+        mapController.value?.setMapStyle(mapStyle);
         isStyleSet = true;
+
+        sizeAnimationController.forward();
       } catch (e) {
         log(e.toString());
       }
@@ -56,7 +72,7 @@ class _MapViewState extends State<MapView> {
             ),
             child: GoogleMap(
               onMapCreated: (controller) {
-                _mapController.value = controller;
+                mapController.value = controller;
               },
               initialCameraPosition: const CameraPosition(
                   target: LatLng(6.4299718, 3.3344001), zoom: 13.0),
@@ -65,76 +81,95 @@ class _MapViewState extends State<MapView> {
           Positioned.fill(
             child: SafeArea(
               child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
                 child: Column(
                   children: [
                     SizedBox(
                       height: 56,
                       child: Row(
                         children: [
-                          const Expanded(child: SearchWidget()),
+                          Expanded(
+                              child: wrapInScaleTransition(
+                                  child: const SearchWidget())),
                           const SizedBox(
                             width: 12,
                           ),
-                          IconButton(
-                            constraints: const BoxConstraints(
-                                minHeight: 56, minWidth: 56),
-                            onPressed: () {},
-                            icon: SvgPicture.asset(
-                              SvgPaths.settings,
-                              height: 20,
-                              width: 20,
-                            ),
-                            style: IconButton.styleFrom(
-                              backgroundColor: Colors.white,
+                          wrapInScaleTransition(
+                            child: IconButton(
+                              constraints: const BoxConstraints(
+                                  minHeight: 56, minWidth: 56),
+                              onPressed: () {},
+                              icon: SvgPicture.asset(
+                                SvgPaths.settings,
+                                height: 20,
+                                width: 20,
+                              ),
+                              style: IconButton.styleFrom(
+                                backgroundColor: Colors.white,
+                              ),
                             ),
                           )
                         ],
                       ),
                     ),
                     const Spacer(),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 14.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                    KeyboardVisibilityBuilder(
+                      builder: (context, isKeyboardVisible) {
+                        if (isKeyboardVisible) {
+                          return const SizedBox();
+                        }
+
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 14.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.end,
                             children: [
-                              BlurryButton(
-                                icon: SvgPicture.asset(SvgPaths.layer,
-                                    color: Colors.white),
-                                boxDecoration:
-                                    const BoxDecoration(shape: BoxShape.circle),
+                              Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  wrapInScaleTransition(
+                                    child: BlurryButton(
+                                      icon: SvgPicture.asset(SvgPaths.layer,
+                                          color: Colors.white),
+                                      boxDecoration:
+                                          const BoxDecoration(shape: BoxShape.circle),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 4,
+                                  ),
+                                  wrapInScaleTransition(
+                                    child: BlurryButton(
+                                      icon: SvgPicture.asset(SvgPaths.send,
+                                          color: Colors.white),
+                                      boxDecoration:
+                                          const BoxDecoration(shape: BoxShape.circle),
+                                    ),
+                                  ),
+                                ],
                               ),
-                              const SizedBox(
-                                height: 4,
-                              ),
-                              BlurryButton(
-                                icon: SvgPicture.asset(SvgPaths.send,
-                                    color: Colors.white),
-                                boxDecoration:
-                                    const BoxDecoration(shape: BoxShape.circle),
+                              wrapInScaleTransition(
+                                child: BlurryButton(
+                                  icon: SvgPicture.asset(
+                                    SvgPaths.menu,
+                                    color: Colors.white,
+                                  ),
+                                  label: "List of variants",
+                                  boxDecoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(30),
+                                  ),
+                                ),
                               ),
                             ],
                           ),
-                          BlurryButton(
-                            icon: SvgPicture.asset(
-                              SvgPaths.menu,
-                              color: Colors.white,
-                            ),
-                            label: "List of variants",
-                            boxDecoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(30),
-                            ),
-                          ),
-                        ],
-                      ),
+                        );
+                      }
                     ),
                     const SizedBox(
-                      height: 120,
+                      height: 100,
                     )
                   ],
                 ),
@@ -145,4 +180,9 @@ class _MapViewState extends State<MapView> {
       ),
     );
   }
+
+  Widget wrapInScaleTransition({required Widget child}) => ScaleTransition(
+        scale: sizeAnimationController,
+        child: child,
+      );
 }
